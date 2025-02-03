f268df1eab0b4fdaea8f04f812866748edcc950a
Maria Camila Calle <mariacamilacalle1@gmail.com>
2025-02-03 14:17:45 -0500

    Server.js ready products route

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
+
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const supabase = require('./db');
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente en Railway');
+app.use(cors());
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+ try {
+   const { data, error } = await supabase
+     .from('products')
+     .select('*');
+
+   if (error) {
+     res.status(400).json({ message: 'Error al consultar productos', error: error.message });
+     return;
+   }
+
+   res.status(200).json(data);
+ } catch (err) {
+   res.status(500).json({
+     message: 'Error interno del servidor al consultar productos',
+     error: err.message,
+   });
+ }
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+ console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
